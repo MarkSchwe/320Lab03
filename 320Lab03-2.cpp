4c07ab0d2fa52f23924addc8b52fa485414c3ac9
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2022-02-21 12:48:46 -0500

    Update 320Lab03-2.cpp

--- 320Lab03-2.cpp
+++ 320Lab03-2.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include "max.h"
+using namespace std;
 
+int main(){
+    srand(time(0));
+    int highest = 0;
+    tnode<int> *head = buildIntTree<int>(2);
+    maxVal(head, highest);
+    cout << highest << endl;
+    displayTree(head, 10);
+    return 0;
+}
