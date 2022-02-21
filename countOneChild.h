44296529451d1c121baab830537b009711336ad4
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2022-02-21 12:49:42 -0500

    Update countOneChild.h

--- countOneChild.h
+++ countOneChild.h
@@ -1,1 +1,28 @@
+#include <iostream>
+#include "d_tnodel.h"
+
+using namespace std;
+
+template<typename T>
+class oneChild{
+public:
+    void countOneChildt(tnode<T>*parent, int &children);
+};
+
+template <typename T>
+void countOneChild(tnode<T>*parent, int &children){
+if(parent->left == nullptr && parent->right != nullptr){
+children++;
+countOneChild(parent->right, children);
+}
+if(parent->left != nullptr && parent->right != nullptr){
+countOneChild(parent->right, children);
+countOneChild(parent->left, children);
+}
+if(parent->left != nullptr && parent->right == nullptr){
+children++;
+countOneChild(parent->left, children);
+}
+}
+
 
