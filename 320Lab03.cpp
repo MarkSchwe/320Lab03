019d38b663ce5f9415205d7e3f8b03c0a08bf88b
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2022-02-21 12:49:16 -0500

    Update 320Lab03.cpp

--- 320Lab03.cpp
+++ 320Lab03.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include "countOneChild.h"
+using namespace std;
 
+int main(){
+    srand(time(0));
+    int children = 0;
+    oneChild<char> one;
+    tnode<char> *root = buildTree(2);
+    countOneChild(root,children);
+    cout << children << endl;
+    displayTree(root, 2);
+    return 0;
+}
